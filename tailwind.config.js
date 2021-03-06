7b2b7fe395281feb89a4029f5e4c97b3a613cfe3
Joshua Galit <joshuaimalay@gmail.com>
2021-03-06 09:26:37 +0800

    feat: header layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans]
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans]
       }
     },
   },
