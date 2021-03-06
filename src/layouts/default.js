cb641d754a03fdf1bd5ec06081749ffa869ef772
Joshua Galit <joshuaimalay@gmail.com>
2021-03-06 12:02:27 +0800

    feat: header menu toggle

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -2,7 +2,7 @@ import NavBar from '~/components/NavBar'
 
 export default function Layout ({ children }) {
   return (
-    <div className="container mx-auto max-w-7xl w-full px-4">
+    <div className="container mx-auto max-w-7xl w-full">
       <NavBar />
       <main>
         { children }
