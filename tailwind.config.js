9c7d5ebc6b245d6a00d591952a401e2b67ccf703
Joshua Galit <joshuaimalay@gmail.com>
2021-03-13 15:46:03 +0800

    fix: delete purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   purge: [
-    './src/pages/**/*.{js,ts,jsx,tsx}', 
-    './src/components/**/*.{js,ts,jsx,tsx}'
+    // './src/pages/**/*.{js,ts,jsx,tsx}', 
+    // './src/components/**/*.{js,ts,jsx,tsx}'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
