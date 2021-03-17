8774d2807d72f77225bde69c7fa6911d4cdc4524
acatzk <joshuaimalay@gmail.com>
2021-03-17 11:59:18 +0800

    refactor: purge css (#21)
    * feat: about us page (#6)
    
    * fix: button responsive
    
    * feat: about us page
    
    * refactor: purge set to false

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: [
-    './src/pages/**/*.{js,ts,jsx,tsx}', 
-    './src/components/**/*.{js,ts,jsx,tsx}'
-  ],
+  purge: false,
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
