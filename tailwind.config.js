6df7e98ab9eafabe42482a0d3636a2350b622765
Joshua Galit <joshuaimalay@gmail.com>
2021-03-19 23:28:52 +0800

    feat: tailwindcss/just-in-time compiler

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
-  purge: false,
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}', 
+    './src/components/**/*.{js,ts,jsx,tsx}'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["Poppins", ...defaultTheme.fontFamily.sans]
-      },
       colors: {
         'primary-indigo': '#6924FF',
         'primary-blue': '#1E2D56',
