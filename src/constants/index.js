2e0c36e2dd04782b5bf3aab266ec62ff2b374a5f
acatzk <joshuaimalay@gmail.com>
2021-04-21 20:52:02 +0800

    chore/update general code (#24)
    * feat: about us page (#6)
    
    * fix: button responsive
    
    * feat: about us page
    
    * chore: update file, code and components

---
+++ src/constants/index.js
@@ -0,0 +1,76 @@
+export const values = [
+  {
+    icon: '/svgs/values/values1.svg',
+    text: 'Research and Discovery'
+  },
+  {
+    icon: '/svgs/values/values2.svg',
+    text: 'Validating and Shaping Idea'
+  },
+  {
+    icon: '/svgs/values/values3.svg',
+    text: 'Design and Prototyping'
+  },
+  {
+    icon: '/svgs/values/values4.svg',
+    text: 'Development'
+  },
+  {
+    icon: '/svgs/values/values5.svg',
+    text: 'Testing and Quality Assurance'
+  },
+  {
+    icon: '/svgs/values/values6.svg',
+    text: 'Maintenance and Support'
+  },
+]
+
+export const services = [
+  {
+    icon: '/images/services/services1.png',
+    text: 'Software Development'
+  },
+  {
+    icon: '/images/services/services2.png',
+    text: 'Multimedia Arts and Design'
+  },
+  {
+    icon: '/images/services/services3.png',
+    text: 'Web Development'
+  },
+  {
+    icon: '/images/services/services4.png',
+    text: 'Web Design & Development'
+  },
+  {
+    icon: '/images/services/services5.png',
+    text: 'User Experience Design'
+  },
+]
+
+export const links = [
+  {
+    text: 'Home',
+    to: '/'
+  },
+  {
+    text: 'Services',
+    to: '/services'
+  },
+  {
+    text: 'About',
+    to: '/about'
+  },
+  {
+    text: 'Case Studies',
+    to: '/case-studies'
+  },
+  {
+    text: 'Careers',
+    to: '/careers'
+  },
+  {
+    text: 'Contact',
+    to: '/contact'
+  }
+]
