4b5b3cc92078a21e8fa17342a836a74b16fafd47
acatzk <joshuaimalay@gmail.com>
2021-04-24 13:33:00 +0800

    feat: add google fonts (#25)
    * feat: about us page (#6)
    
    * fix: button responsive
    
    * feat: about us page
    
    * feat: add google fonts

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -4,7 +4,7 @@ import Footer from '~/components/Footer'
 export default function Layout ({ children }) {
   return (
     <>
-      <div className="block lg:hidden absolute -top-32 -left-40">
+      <div className="font-sans block lg:hidden absolute -top-32 -left-40">
         <img src="/svgs/buble.svg"  />
       </div>
       <div className="relative mx-auto max-w-7xl w-full">
