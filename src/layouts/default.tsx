3608ac78f3c2bcccf63e530c22dadccfb6229986
acatzk <joshuaimalay@gmail.com>
2021-06-17 13:13:17 +0800

    fix: optimize images with next/image (#30)

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import NavBar from '~/components/NavBar'
 import Footer from '~/components/Footer'
 
@@ -5,7 +6,13 @@ export default function Layout ({ children }) {
   return (
     <>
       <div className="font-sans block lg:hidden absolute -top-32 -left-40">
-        <img src="/svgs/buble.svg"  />
+        <Image 
+          src="/svgs/buble.svg"  
+          width={1000}
+          height={1000}
+          quality={25}
+          layout="intrinsic"
+        />
       </div>
       <div className="relative mx-auto max-w-7xl w-full">
         <NavBar />
